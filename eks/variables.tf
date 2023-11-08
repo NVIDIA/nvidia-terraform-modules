c3b1bb3451970172c6d471d4b40dbce54222ca0c
Sunil Upare <supare@ncsu.edu>
2023-11-08 10:35:01 -0800

    Updated private and public subnet ranges for VPC in EKS

--- eks/variables.tf
+++ eks/variables.tf
@@ -210,13 +210,13 @@ variable "additional_user_data" {
 variable "private_subnets" {
   type        = list(any)
   description = "List of subnet ranges for the Holoscan VPC"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
 }
 
 variable "public_subnets" {
   type        = list(any)
   description = "List of subnet ranges for the Holoscan VPC"
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  default     = ["10.0.96.0/19", "10.0.128.0/19", "10.0.160.0/19"]
 }
 
 variable "ssh_key" {
