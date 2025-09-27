9bc77e1c9989b936d2186724c599ad9369ff4735
Rin <rin@picturestudio.ai>
2025-09-26 22:49:33 -0400

    pin ebs_csi_irsa_role module version to 5.43.0, ignore tf lockfile
    Signed-off-by: Rinchin Shoysoronov <shoysoronovr@gmail.com>

--- eks/main.tf
+++ eks/main.tf
@@ -144,6 +144,7 @@ module "eks" {
 ********************************************/
 module "ebs_csi_irsa_role" {
   source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version               = "5.43.0"
   role_name             = "${var.cluster_name}-ebs-csi"
   attach_ebs_csi_policy = true
   oidc_providers = {
