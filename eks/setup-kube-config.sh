ae0ad1e8cd1758efe70d6dce853256fa0238f368
Francis Guillier US <89548646+francisguillier@users.noreply.github.com>
2026-08-18 17:23:45 -0700

    Update setup-kube-config.sh

--- eks/setup-kube-config.sh
+++ eks/setup-kube-config.sh
@@ -3,7 +3,10 @@
 
 #!/bin/bash
 
-eval "$(jq -r '@sh "_aws_profile=\(.aws_profile) _aws_region=\(.aws_region) _eks_cluster_name=\(.eks_cluster_name) _eks_cluster_arn=\(.eks_cluster_arn)"')"
+_aws_profile=$(echo "$input" | jq -r '.aws_profile')
+_aws_region=$(echo "$input" | jq -r '.aws_region')
+_eks_cluster_name=$(echo "$input" | jq -r '.eks_cluster_name')
+_eks_cluster_arn=$(echo "$input" | jq -r '.eks_cluster_arn')
 
 if aws --profile "${_aws_profile}" eks update-kubeconfig --region "${_aws_region}" --name "${_eks_cluster_name}" 1> /dev/null 2>&1; then
   jq -n --arg command_to_use "kubectl config use-context ${_eks_cluster_arn}" '{"status":"success","commandToUse":$command_to_use}'
